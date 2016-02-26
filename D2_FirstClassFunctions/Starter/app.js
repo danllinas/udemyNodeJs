1b594149ac2ce80dd25608cfcc6551fe2fee65d3
Daniel Llinas <danllinas@gmail.com>
2016-02-25 22:05:23 -0500

    Finished another section.

---
+++ D2_FirstClassFunctions/Starter/app.js
@@ -0,0 +1,30 @@
+// Your Javascript Code Goes He
+//functionstatement
+function greet() {
+  console.log('hi');
+}
+greet();
+
+//functions are first class
+function logGreeting(fn) {
+  fn();
+}
+
+logGreeting(greet);
+
+//function expression
+var greetMe = function() {
+  console.log('Hi, Danny');
+}
+
+greetMe();
+
+
+//it's first class
+logGreeting(greetMe);
+
+
+//use a function expression on the fly
+logGreeting(function(){
+  console.log('Hello, Dan!');
+});
