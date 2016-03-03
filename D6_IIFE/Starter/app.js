31534d3f6c6791441583151be5769cac43e83945
Daniel Llinas <danllinas@gmail.com>
2016-03-03 17:05:58 -0500

    More progress.

---
+++ D6_IIFE/Starter/app.js
@@ -0,0 +1,15 @@
+// Your Javascript Code Goes Here
+var firstName = "Duncan";
+
+(function(lastname){
+  
+  var firstName = "Danny";
+  console.log(firstName);
+  console.log(lastname);
+  
+  
+  
+}("Llinas"));
+
+
+console.log(firstName);
